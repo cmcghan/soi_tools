0269b309890dc82752db7a39757a587b3f879711
Catharine McGhan <cat.mcghan@uc.edu>
2017-08-02 15:21:00 -0400

    changes to bash scripts

--- run_02Ex_with_ros.sh
+++ run_02Ex_with_ros.sh
@@ -3,41 +3,79 @@
 # https://github.com/cmcghan/soi_tools
 # Additional copyright may be held by others, as reflected in the commit history.
 
-gnome-terminal -e 'bash -c "echo lmcp2rosmsg.py + caktin_make + roscore; ./lmcp2rosmsg/lmcp2rosmsg.py dir /home/$USER/UxAS_$USER/OpenUxAS/mdms; cd catkin_lmcp; source devel/setup.bash; roscore; read line"'
-
-while [ 1 ]
-do
-    if [ `ps -A | grep -c roscore` -eq 1 ]; then
-        break
-    else
-        echo "Waiting until roscore is up before proceeding..."
-        sleep 2
-    fi
-done
-
 # do NOT put () in bash -c call, will not work!
-gnome-terminal -e 'bash -c "echo rosbridge_server - port 9090; cd catkin_lmcp; source devel/setup.bash; roslaunch ../../rss_git_lite/rosbridge_server_9090.launch; read line"'
 
-while [ 1 ]
+# references for looping over arrays:
+# * https://stackoverflow.com/questions/8880603/loop-through-an-array-of-strings-in-bash
+# * http://tldp.org/LDP/abs/html/loops1.html
+# reference for bash strings to gnome-terminal -e:
+# * https://stackoverflow.com/questions/32276623/bash-passing-strings-to-gnome-terminal-e
+
+
+if [ $# -lt 1 ] || [ [ "$1" != "ros" ] && [ "$1" != "nomake" ] && [ "$1" != "tbot" ] && [ "$1" != "nomaketbot" ] ]; then
+    echo "This script requires at least one valid argument!"
+    echo "Acceptable arguments are: 'ros' 'nomake' 'tbot' 'nomaketbot'"
+fi
+
+if [ "$1" == "ros" ] || [ "$1" == "tbot" ]; then
+    # "cd commands; source and other commands" "run command" "label"
+    PAUSERUN=("./lmcp2rosmsg/lmcp2rosmsg.py dir /home/$USER/UxAS_$USER/OpenUxAS/mdms; cd catkin_lmcp; catkin_make; source devel/setup.bash" "roscore" "roscore")
+elif [ "$1" == "nomake" ] || [ "$1" == "nomaketbot" ]; then
+    # "cd commands; source and other commands" "run command" "label"
+    PAUSERUN=("cd catkin_lmcp; source devel/setup.bash" "roscore" "roscore")
+fi
+PAUSERUN+=("cd catkin_lmcp; source devel/setup.bash" "roslaunch ../../rss_git_lite/rosbridge_server_9090.launch" "roslaunch")
+
+# "cd commands; source and other commands" "run command"
+LISTRUN=(
+"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/MissionCommand"
+"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/LineSearchTask"
+"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/VehicleActionCommand"
+)
+if [ "$1" == "ros" ] || [ "$1" == "nomake" ]; then
+    LISTRUN+=(
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch.sh"
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runAMASE_WaterwaySearch.sh"
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "python ./ros_adapter.py"
+    )
+elif [ "$1" == "tbot" ] || [ "$1" == "nomaketbot" ]; then
+    # turtlebot calls modified from: http://learn.turtlebot.com/2015/02/03/7/
+
+    # "cd commands; source and other commands" "run command"
+    LISTRUN+=(
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch.sh"
+    "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/opt/ros/kinetic/share/turtlebot_gazebo/worlds/empty.world"
+#    "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo amcl_demo.launch"
+#    "cd catkin_lmcp; source devel/setup.bash" "rosrun tf tf_echo /map /base_link"
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "python ./ros_adapter.py"
+    )
+fi
+#echo LISTRUN is $LISTRUN
+#echo LISTRUN(all) is ${LISTRUN[@]}
+
+# length via ${#name[@]}
+for (( i=0; i<${#PAUSERUN[@]}; i+=3 ))
 do
-    if [ `ps -A | grep -c roslaunch` -eq 1 ]; then
-        break
-    else
-        echo "Waiting until roslaunch of rosbridge_suite is up before proceeding..."
-        sleep 2
-    fi
+    eval "gnome-terminal -e 'bash -c \"echo ${PAUSERUN[$i+1]}; ${PAUSERUN[$i]}; ${PAUSERUN[$i+1]}; read line\"' "
+
+    while [ 1 ]
+    do
+        if [ `ps -A | grep -c ${PAUSERUN[$i+2]}` -eq 1 ]; then
+            break
+        else
+            echo "Waiting until ${PAUSERUN[$i+2]} is up before proceeding..."
+            sleep 2
+        fi
+    done
 done
 
-gnome-terminal -e 'bash -c "echo rostopic /from_uxas/MissionCommand; cd catkin_lmcp; source devel/setup.bash; rostopic echo /from_uxas/MissionCommand; read line"'
-
-gnome-terminal -e 'bash -c "echo rostopic /from_uxas/LineSearchTask; cd catkin_lmcp; source devel/setup.bash; rostopic echo /from_uxas/LineSearchTask; read line"'
-
-gnome-terminal -e 'bash -c "echo rostopic /from_uxas/VehicleActionCommand; cd catkin_lmcp; source devel/setup.bash; rostopic echo /from_uxas/VehicleActionCommand; read line"'
-
-gnome-terminal -e 'bash -c "echo runUxAS_WaterwaySearch.sh; cd ../OpenUxAS/examples/02_Example_WaterwaySearch; ./runUxAS_WaterwaySearch.sh; read line"'
-
-gnome-terminal -e 'bash -c "echo runAMASE_WaterwaySearch.sh; cd ../OpenUxAS/examples/02_Example_WaterwaySearch; ./runAMASE_WaterwaySearch.sh; read line"'
-
-gnome-terminal -e 'bash -c "echo rosadapter.py; cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger; python ./ros_adapter.py; read line"'
+# length via ${#name[@]}
+for (( i=0; i<${#LISTRUN[@]}; i+=2 ))
+do
+    #RUNTHIS="-e 'bash -c \"echo ${LISTRUN[$i+1]}; ${LISTRUN[$i]}; ${LISTRUN[$i+1]}; read line\"' "
+    #eval "gnome-terminal "$RUNTHIS
+    eval "gnome-terminal -e 'bash -c \"echo ${LISTRUN[$i+1]}; ${LISTRUN[$i]}; ${LISTRUN[$i+1]}; read line\"' "
+done
 
 # --eof--
+
--- run_02Ex_with_ros_nomake.sh
+++ run_02Ex_with_ros_nomake.sh
@@ -11,10 +11,47 @@
 # reference for bash strings to gnome-terminal -e:
 # * https://stackoverflow.com/questions/32276623/bash-passing-strings-to-gnome-terminal-e
 
-PAUSERUN=(
-"cd catkin_lmcp; source devel/setup.bash" "roscore" "roscore"
-"cd catkin_lmcp; source devel/setup.bash" "roslaunch ../../rss_git_lite/rosbridge_server_9090.launch" "roslaunch"
+
+if [ $# -lt 1 ] || [ [ "$1" != "ros" ] && [ "$1" != "nomake" ] && [ "$1" != "tbot" ] && [ "$1" != "nomaketbot" ] ]; then
+    echo "This script requires at least one valid argument!"
+    echo "Acceptable arguments are: 'ros' 'nomake' 'tbot' 'nomaketbot'"
+fi
+
+if [ "$1" == "ros" ] || [ "$1" == "tbot" ]; then
+    # "cd commands; source and other commands" "run command" "label"
+    PAUSERUN=("./lmcp2rosmsg/lmcp2rosmsg.py dir /home/$USER/UxAS_$USER/OpenUxAS/mdms; cd catkin_lmcp; catkin_make; source devel/setup.bash" "roscore" "roscore")
+elif [ "$1" == "nomake" ] || [ "$1" == "nomaketbot" ]; then
+    # "cd commands; source and other commands" "run command" "label"
+    PAUSERUN=("cd catkin_lmcp; source devel/setup.bash" "roscore" "roscore")
+fi
+PAUSERUN+=("cd catkin_lmcp; source devel/setup.bash" "roslaunch ../../rss_git_lite/rosbridge_server_9090.launch" "roslaunch")
+
+# "cd commands; source and other commands" "run command"
+LISTRUN=(
+"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/MissionCommand"
+"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/LineSearchTask"
+"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/VehicleActionCommand"
 )
+if [ "$1" == "ros" ] || [ "$1" == "nomake" ]; then
+    LISTRUN+=(
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch.sh"
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runAMASE_WaterwaySearch.sh"
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "python ./ros_adapter.py"
+    )
+elif [ "$1" == "tbot" ] || [ "$1" == "nomaketbot" ]; then
+    # turtlebot calls modified from: http://learn.turtlebot.com/2015/02/03/7/
+
+    # "cd commands; source and other commands" "run command"
+    LISTRUN+=(
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch.sh"
+    "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/opt/ros/kinetic/share/turtlebot_gazebo/worlds/empty.world"
+#    "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo amcl_demo.launch"
+#    "cd catkin_lmcp; source devel/setup.bash" "rosrun tf tf_echo /map /base_link"
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "python ./ros_adapter.py"
+    )
+fi
+#echo LISTRUN is $LISTRUN
+#echo LISTRUN(all) is ${LISTRUN[@]}
 
 # length via ${#name[@]}
 for (( i=0; i<${#PAUSERUN[@]}; i+=3 ))
@@ -31,17 +68,6 @@ do
         fi
     done
 done
-
-LISTRUN=(
-"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/MissionCommand"
-"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/LineSearchTask"
-"cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/VehicleActionCommand"
-"cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch.sh"
-"cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runAMASE_WaterwaySearch.sh"
-"cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "python ./ros_adapter.py"
-)
-#echo LISTRUN is $LISTRUN
-#echo LISTRUN(all) is ${LISTRUN[@]}
 
 # length via ${#name[@]}
 for (( i=0; i<${#LISTRUN[@]}; i+=2 ))
