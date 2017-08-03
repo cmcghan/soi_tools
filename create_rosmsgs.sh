231ae4f994548c0df916acd346931a439d379feb
Catharine McGhan <cat.mcghan@uc.edu>
2017-08-03 15:18:03 -0400

    generalizing a bit more

--- create_rosmsgs.sh
+++ create_rosmsgs.sh
@@ -3,9 +3,9 @@
 # https://github.com/cmcghan/soi_tools
 # Additional copyright may be held by others, as reflected in the commit history.
 
-echo "creating ROS .msg files from MDMs in '/home/$USER/UxAS_cmcghan/OpenUxAS/mdms'..." 
+echo "creating ROS .msg files from MDMs in '/home/$USER/UxAS_$USER/OpenUxAS/mdms'..." 
 echo "and storing them in directory ./catkin_lmcp (at the root of whatever directory this script is run from!)"
-./lmcp2rosmsg/lmcp2rosmsg.py dir /home/$USER/UxAS_cmcghan/OpenUxAS/mdms
+./lmcp2rosmsg/lmcp2rosmsg.py dir /home/$USER/UxAS_$USER/OpenUxAS/mdms
 echo " "
 echo "All done!"
 echo "Remember to run 'catkin_make' in the 'catkin_lmcp' directory for ROS to see what's going on!"
--- run_02Ex_with_ros.sh
+++ run_02Ex_with_ros.sh
@@ -21,21 +21,31 @@ fi
 #
 # set properties for determining what should run below
 #
+# catkin_make = {'yes','no'}
+# amasevsgazebo = {'amase','gazebo'}
+# adapterparam = {'amase','intertest','turtlebot'}
+# 
 if [ "$1" == "ros" ]; then
     catkinmake='yes'
     amasevsgazebo='amase'
+    adapterparam='test'
     
 elif [ "$1" == "nomake" ]; then
     catkinmake='no'
     amasevsgazebo='amase'
+    adapterparam='amase'
 
 elif [ "$1" == "tbot" ]; then
     catkinmake='yes'
     amasevsgazebo='gazebo'
+    adapterparam='intertest'
+    #adapterparam='turtlebot'
 
 elif [ "$1" == "nomaketbot" ]; then
     catkinmake='no'
     amasevsgazebo='gazebo'
+    adapterparam='intertest'
+    #adapterparam='turtlebot'
 
 fi
 
@@ -44,7 +54,8 @@ fi
 # PAUSERUN=("cd commands; source and other commands" "run command" "label")
 #
 if [ "$catkinmake" == "yes" ]; then
-    PAUSERUN=("./lmcp2rosmsg/lmcp2rosmsg.py dir /home/$USER/UxAS_$USER/OpenUxAS/mdms; cd catkin_lmcp; catkin_make; source devel/setup.bash" "roscore" "roscore")
+    #PAUSERUN=("./lmcp2rosmsg/lmcp2rosmsg.py dir /home/$USER/UxAS_$USER/OpenUxAS/mdms; cd catkin_lmcp; catkin_make; source devel/setup.bash" "roscore" "roscore")
+    PAUSERUN=("./create_rosmsgs.sh; cd catkin_lmcp; catkin_make; source devel/setup.bash" "roscore" "roscore")
 elif [ "$catkinmake" == "no" ]; then
     PAUSERUN=("cd catkin_lmcp; source devel/setup.bash" "roscore" "roscore")
 fi
@@ -58,6 +69,8 @@ LISTRUN=(
 "cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/MissionCommand"
 "cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/LineSearchTask"
 "cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/VehicleActionCommand"
+"cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "./ext_logger_cli.py afrl.cmasi.SessionStatus"
+"cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "./ext_logger_cli.py afrl.cmasi.AirVehicleState"
 )
 if [ "$amasevsgazebo" == "amase" ]; then
     LISTRUN+=(
@@ -69,24 +82,38 @@ elif [ "$amasevsgazebo" == "gazebo" ]; then
 
     # "cd commands; source and other commands" "run command"
     LISTRUN+=(
+    "cd catkin_lmcp; source devel/setup.bash" "rostopic echo /from_uxas/MissionCommand/waypointlist"
+    # below requires the ROS .msg --> UxAS MDMs converter (opposite direction)
+    #"cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "./ext_logger_cli.py rosgraph.Clock.clock" # ['/clock','rosgraph_msgs/Clock'],
+    #"cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "./ext_logger_cli.py nav.Odometry.odom" # ['/odom','nav_msgs/Odometry'],
+    #"cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "./ext_logger_cli.py sensor.Imu.mobile_base.sensors.imu_data" # ['/mobile_base/sensors/imu_data','sensor_msgs/Imu'],
+    #"cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "./ext_logger_cli.py std.Int64.curwaypt" # ['/curwaypt','std_msgs/Int64']]
     "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch.sh"
-    "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/opt/ros/kinetic/share/turtlebot_gazebo/worlds/empty.world"
-#    "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo amcl_demo.launch"
-#    "cd catkin_lmcp; source devel/setup.bash" "rosrun tf tf_echo /map /base_link"
     )
+    if [ "$adapterparam" == "intertest" ]; then # will get removed later...
+        LISTRUN+=("cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runAMASE_WaterwaySearch.sh")
+    elif [ "$adapterparam" == "turtlebot" ]; then
+        LISTRUN+=(
+                  "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/opt/ros/kinetic/share/turtlebot_gazebo/worlds/empty.world"
+##        "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo amcl_demo.launch"
+##        "cd catkin_lmcp; source devel/setup.bash" "rosrun tf tf_echo /map /base_link"
+                 )
+    fi
 fi
-LISTRUN+=("cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "python ./ros_adapter.py")
+LISTRUN+=("cd ../OpenUxAS/examples/02_Example_WaterwaySearch/ext_logger" "python ./ros_adapter.py $adapterparam")
 #echo LISTRUN is $LISTRUN
 #echo LISTRUN(all) is ${LISTRUN[@]}
 #echo LISTRUN length is ${#LISTRUN[@]}
+
+# source ./setup-gnome-terminal-titling.sh; set-title the new title # this works in a normal terminal, but doesn't seem to be working with the gnome-terminal instance...
 
 #
 # run PAUSERUN list stuff one at a time, with pauses after each (waiting for completion) in new gnome-terminal windows
 #
 for (( i=0; i<${#PAUSERUN[@]}; i+=3 ))
 do
-    eval "gnome-terminal -e 'bash -c \"echo ${PAUSERUN[$i+1]}; ${PAUSERUN[$i]}; ${PAUSERUN[$i+1]}; read line\"' "
-
+    eval "gnome-terminal -e 'bash -c \"echo ${PAUSERUN[$i+1]}; ${PAUSERUN[$i]}; ${PAUSERUN[$i+1]}; read line\"' "    
+    
     while [ 1 ]
     do
         if [ `ps -A | grep -c ${PAUSERUN[$i+2]}` -eq 1 ]; then
@@ -109,4 +136,3 @@ do
 done
 
 # --eof--
-
