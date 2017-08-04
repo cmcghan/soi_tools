906c33c3486906c5a812c6cb74b1dd34caa06bdb
Catharine McGhan <cat.mcghan@uc.edu>
2017-08-04 11:22:28 -0400

    working on ROS single-vehicle example based on example 02 WaterwaySearch

--- run_02Ex_with_ros.sh
+++ run_02Ex_with_ros.sh
@@ -12,9 +12,9 @@
 # * https://stackoverflow.com/questions/32276623/bash-passing-strings-to-gnome-terminal-e
 
 # check that there is at least one commandline argument and that it's an acceptable 'switch'
-if [ $# -lt 1 ] || ( [ "$1" != "ros" ] && [ "$1" != "nomake" ] && [ "$1" != "tbot" ] && [ "$1" != "nomaketbot" ] ); then
+if [ $# -lt 1 ] || ( [ "$1" != "ros" ] && [ "$1" != "nomake" ] && [ "$1" != "nomake1veh" ] && [ "$1" != "tbot" ] && [ "$1" != "nomaketbot" ] ); then
     echo "This script requires at least one valid argument! (arg='$1')"
-    echo "Acceptable arguments are: 'ros' 'nomake' 'tbot' 'nomaketbot'"
+    echo "Acceptable arguments are: 'ros' 'nomake' 'nomake1veh' 'tbot' 'nomaketbot'"
     exit
 fi
 
@@ -22,7 +22,7 @@ fi
 # set properties for determining what should run below
 #
 # catkin_make = {'yes','no'}
-# amasevsgazebo = {'amase','gazebo'}
+# amasevsgazebo = {'amase','amase1veh','gazebo'}
 # adapterparam = {'amase','intertest','turtlebot'}
 # 
 if [ "$1" == "ros" ]; then
@@ -34,6 +34,12 @@ elif [ "$1" == "nomake" ]; then
     catkinmake='no'
     amasevsgazebo='amase'
     adapterparam='amase'
+
+elif [ "$1" == "nomake1veh" ]; then
+    catkinmake='no'
+    amasevsgazebo='amase1veh'
+    adapterparam='intertest'
+    #adapterparam='turtlebot'
 
 elif [ "$1" == "tbot" ]; then
     catkinmake='yes'
@@ -77,6 +83,11 @@ if [ "$amasevsgazebo" == "amase" ]; then
     "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch.sh"
     "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runAMASE_WaterwaySearch.sh"
     )
+elif [ "$amasevsgazebo" == "amase1veh" ]; then
+    LISTRUN+=(
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runUxAS_WaterwaySearch_ros.sh"
+    "cd ../OpenUxAS/examples/02_Example_WaterwaySearch" "./runAMASE_WaterwaySearch_ros.sh"
+    )
 elif [ "$amasevsgazebo" == "gazebo" ]; then
     # turtlebot calls modified from: http://learn.turtlebot.com/2015/02/03/7/
 
@@ -95,8 +106,8 @@ elif [ "$amasevsgazebo" == "gazebo" ]; then
     elif [ "$adapterparam" == "turtlebot" ]; then
         LISTRUN+=(
                   "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/opt/ros/kinetic/share/turtlebot_gazebo/worlds/empty.world"
-##        "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo amcl_demo.launch"
-##        "cd catkin_lmcp; source devel/setup.bash" "rosrun tf tf_echo /map /base_link"
+##                "cd catkin_lmcp; source devel/setup.bash" "roslaunch turtlebot_gazebo amcl_demo.launch"
+##                "cd catkin_lmcp; source devel/setup.bash" "rosrun tf tf_echo /map /base_link"
                  )
     fi
 fi
